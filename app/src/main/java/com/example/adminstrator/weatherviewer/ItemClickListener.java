b74d1adb183e117771981b6bbce59aaea89a3fae
Richard Ekong <ekong.richard@yahoo.com>
2017-09-21 22:22:00 +0300

    added ItemClickListener interface for recyclerview

---
+++ app/src/main/java/com/example/adminstrator/weatherviewer/ItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.adminstrator.weatherviewer;
+
+import android.view.View;
+
+/**
+ * Created by Adminstrator on 9/21/2017.
+ */
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
